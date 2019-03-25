3f1d750edb00bbc22b043393acf26b954187dfe4
andy <dreamrrrr@hotmail.com>
2019-03-25 16:05:26 +0800

    update django

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\andy.chen\pycharmprojects\hello_django\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
